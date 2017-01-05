ae4460f91f0b8bb27ebbae9d6bfc62d7a1cc2260
wenghengcong <wenghengcong@gmail.com>
2017-01-05 10:33:13 +0800

    ◉功能【增加Content Extension的说明】◉代码【】◉Bug【】

--- iOSNotifications/NotificationContent/NotificationViewController.h
+++ iOSNotifications/NotificationContent/NotificationViewController.h
@@ -12,6 +12,14 @@
 #import <UserNotificationsUI/UserNotificationsUI.h>
 
 //Content Extension 用来修改通知弹出的界面，并且通过category来进行加以定制。
+/*假如带小图文的通知，通知界面未经过这里修改，则会展示成带图片的通知
+未经过Content Extension：
+  1.通知不带图片，直接显示title/body的通知；
+  2.通知带图片，显示为title/图片/body的通知；
+ 
+经过Content Extension，直接加载定制的通知界面。
+*/
+
 
 /*
  
